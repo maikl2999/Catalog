f1d636dbd7d906b945304f9bb08d02d34b5afbd9
maikl2999 <maikl2929@gmail.com>
2019-09-27 18:25:19 +0300

    create product cards

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
-const ProductDetail = () => {
+const ProductDetail = (props) => {
     return (
-        <div>Product Detail</div>
+        <div className="">
+        <div className="card" >
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+            <h5 className="card-title">{props.title}</h5>
+            <p className="card-text">This is a widlead-in to additional content. This content is a little bit longer.</p>
+            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+        </div>
     );
 }
 
--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -6,11 +6,11 @@ class ProductsList extends React.Component {
     renderGoods = () => {
         
         // delete
-        const testProducts = [ 'product', 'product', 'product', 'product',] 
+        const testProducts = [ { title: 'product1', id: 1 }, { title: 'product2', id: 2 }, { title: 'product3', id: 3 },{ title: 'product4', id: 4 },{ title: 'product5', id: 5 }] 
         
-        return testProducts.map(prod => {
+        return testProducts.map(({ title, id }) => {
             return (
-                <ProductDetail title={prod} />
+                <ProductDetail title={title} key={id}/>
             );
         })
     
@@ -18,8 +18,7 @@ class ProductsList extends React.Component {
 
     render() {    
         return (
-            <div>
-                Products List
+            <div className="card-columns">
                 {this.renderGoods()}
             </div>
         );
